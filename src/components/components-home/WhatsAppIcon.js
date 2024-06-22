9acb33f64cf0abe54fb2b61c386c76490a7d78e0
AbdullahAmer30 <abdullahamer392@gmail.com>
2024-06-23 00:39:07 +0500

    new change

--- src/components/components-home/WhatsAppIcon.js
+++ src/components/components-home/WhatsAppIcon.js
@@ -4,7 +4,7 @@ import icon from './whatsapp.png'
 
 const WhatsAppIcon = () => {
     return (
-        <a href="https://wa.me/03308581555" className="whatsapp-icon" target="_blank" rel="noopener noreferrer">
+        <a href="https://wa.me/+923308581555" className="whatsapp-icon" target="_blank" rel="noopener noreferrer">
             <img src={icon} alt="WhatsApp" />
         </a>
     );
